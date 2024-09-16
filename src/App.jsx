a4ef4897e0641b797e0f763b1549bd3c25fa35a3
DileepKumarRambarki <rambarkidileepkumar@gmail.com>
2024-09-16 21:38:52 +0530

    signup page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AlumniPage from './alumnipage'
 import Login from './login';
-
+import Signup from "./SignupPage.jsx"
 
 
 
@@ -14,6 +14,7 @@ function App() {
       <Routes location = {location} key = {location.pathname}>
         <Route path = '/' element = {<AlumniPage />} />
         <Route path = '/login' element = {<Login />} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
 
     </Router>
