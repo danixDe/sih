92b5b6f219d10d747805d742090dc59a58e8365b
danixDe <150506676+danixDe@users.noreply.github.com>
2024-09-22 18:04:18 +0530

    k

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import Layout from './Layout';
 import Home from './pages/home';
 import Login from './login';
 import Signup from './SignupPage.jsx';
-import Signup from "./SignupPage.jsx"
 import Events from './pages/Events';
 import Mentorship from './pages/Members';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import DonationForm from './pages/DonationForm.jsx';
-import Profile from './pages/Profile';  // Import the Profile page
+import Profile from './pages/Profile';  
 
 function App() {
   return (
@@ -24,7 +23,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/donation" element={<DonationForm />} />
-          <Route path="/profile/:id" element={<Profile />} /> {/* New dynamic route */}
+          <Route path="/profile/:id" element={<Profile />} /> 
         </Route>
       </Routes>
     </Router>
